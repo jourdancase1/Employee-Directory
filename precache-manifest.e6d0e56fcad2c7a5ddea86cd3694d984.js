a87991000b8417e235fcb1797c8b8074249d1058
user <user@Jourdans-iMac.lan>
2020-10-26 16:05:59 -0500

    Updates

---
+++ precache-manifest.e6d0e56fcad2c7a5ddea86cd3694d984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2458968c1e4b730cd5b52b8f48b974",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "4b73460e15c112c372c6",
+    "url": "/Employee-Directory/static/css/main.6c18f111.chunk.css"
+  },
+  {
+    "revision": "5b88b8a675a05cd7231d",
+    "url": "/Employee-Directory/static/js/2.69320e98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Employee-Directory/static/js/2.69320e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b73460e15c112c372c6",
+    "url": "/Employee-Directory/static/js/main.790c6024.chunk.js"
+  },
+  {
+    "revision": "b3ad176c9c5c5e85a50b",
+    "url": "/Employee-Directory/static/js/runtime-main.5c5d95e7.js"
+  }
+]);
